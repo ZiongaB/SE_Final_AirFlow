7db75ff0e9f6c83f4b067ec79e15a758a96aa2ec
SebastianMark32 <smarkgolfer@gmail.com>
2023-04-15 18:32:09 -0400

    Setup a backend

---
+++ Backend/index.ts
@@ -0,0 +1,20 @@
+
+// function with types 
+import express, {Express, Request, Response} from 'express'
+import * as dotenv from 'dotenv'
+
+dotenv.config();
+console.log(process.env.PORT)
+
+// app is of type express 
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res:Response) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    // back ticks here 
+    console.log(`Example app listening on port ${port}`)
+})
