5f6eef32e628857f524bae3420d19349a21e8e85
SebastianMark32 <smarkgolfer@gmail.com>
2023-04-21 20:59:44 -0400

    Slight navbar change

---
+++ Backend/routes/trip 2.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const { body } = require('express-validator');
+
+const eventController = require ('../controllers/trip');
+
+const auth =  require('../middleware/auth');
+
+const router = express.Router();
+
+router.get('/', auth, eventController.fetchAll);
+
+router.post(
+    '/',
+    [
+      auth,
+      body('userId').trim().isLength({ min: 1 }).not().isEmpty(),
+      body('tripname').trim().isLength({ min: 1 }).not().isEmpty(),
+      body('parking').trim().isLength({ min: 1 }).not().isEmpty(),
+    ],
+    eventController.postTrip
+);
+
+module.exports = router;
