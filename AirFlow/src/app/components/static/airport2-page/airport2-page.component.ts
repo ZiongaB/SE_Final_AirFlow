aa4e9ca11e09ae93f7cf223e4124d823d770586f
ZiongaB <91677985+ZiongaB@users.noreply.github.com>
2023-05-06 20:29:28 -0400

    Created Tampa  Airport Page

--- AirFlow/src/app/components/static/airport2-page/airport2-page.component.ts
+++ AirFlow/src/app/components/static/airport2-page/airport2-page.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class Airport2PageComponent {
 
+  mainTerminal: boolean = false;
+  aTerminal: boolean = false;
+  cTerminal: boolean = false;
+  eTerminal: boolean = false;
+  fTerminal: boolean = false;
+
+
+  short: boolean = false;
+  long: boolean = false;
+  economy: boolean = false;
+  cell: boolean = false;
+  valet: boolean = false;
+
+  
+
 }
