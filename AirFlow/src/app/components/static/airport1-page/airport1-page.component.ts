6b18f7ffb012aadcb6c43dac4356558aa3390e74
SebastianMark32 <smarkgolfer@gmail.com>
2023-05-07 16:51:38 -0400

    SRQ Airport Page

--- AirFlow/src/app/components/static/airport1-page/airport1-page.component.ts
+++ AirFlow/src/app/components/static/airport1-page/airport1-page.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './airport1-page.component.html',
   styleUrls: ['./airport1-page.component.scss']
 })
+
+
+
+
 export class Airport1PageComponent {
+  short: boolean = false;
+  long: boolean = false;
+  shaded: boolean = false;
+  overflow: boolean = false;
 
 }
